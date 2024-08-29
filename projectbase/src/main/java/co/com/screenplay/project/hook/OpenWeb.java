b0923a3cefa9f8b9fc251421fea3ead8015d6758
Jtorresc1807 <127152588+Jtorresc1807@users.noreply.github.com>
2024-08-29 17:54:16 -0500

    Update OpenWeb.java

--- projectbase/src/main/java/co/com/screenplay/project/hook/OpenWeb.java
+++ projectbase/src/main/java/co/com/screenplay/project/hook/OpenWeb.java
@@ -11,25 +11,25 @@ import net.thucydides.core.util.EnvironmentVariables;
 import static co.com.screenplay.project.utils.Constants.WEB_URL;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 //3
-public class OpenWeb implements Task {   // interfaz que define tareas que los actores pueden realizar.
+public class OpenWeb implements Task {   // 1 interfaz que define tareas que los actores pueden realizar.
 
-    private EnvironmentVariables environmentVariables;      // Permite que OpenWeb acceda a las variables de entorno en serenity.conf
+    private EnvironmentVariables environmentVariables;      // 4 Permite que OpenWeb acceda a las variables de entorno en serenity.conf
 
-    public OpenWeb(String webUrl) {     // Constructor que inicializa objetos
+    public OpenWeb(String webUrl) {     // 8 Constructor que inicializa objetos
         this.webUrl = webUrl;
     }
 
-    private String webUrl;   //Almacenará la URL del sitio web.
+    private String webUrl;   // 7 Almacenará la URL del sitio web.
 
-    @Override           // Está sobrescribiendo un método de la interfaz Task
+    @Override           // 2 Está sobrescribiendo un método de la interfaz Task
     @Step("{0} abre el navegador web")
-    public <T extends Actor> void performAs(T actor) {  // Puede trabajar con cualquier tipo de Actor
+    public <T extends Actor> void performAs(T actor) {  // 2 Puede trabajar con cualquier tipo de Actor
         String pathWebUrl = EnvironmentSpecificConfiguration.from(environmentVariables).getProperty(WEB_URL);
-        //Obtiene la URL del sitio web en la clase Constants.java
-        actor.attemptsTo(Open.url(pathWebUrl));   // El actor intenta abrir la URL obtenida de las variables de entorno.
+        // 5 Obtiene la URL del sitio web en la clase Constants.java
+        actor.attemptsTo(Open.url(pathWebUrl));   // 6 El actor intenta abrir la URL obtenida de las variables de entorno.
     }
 
-    public static Performable browserURL(String webUrl) {  //Crear una nueva instancia de OpenWeb
+    public static Performable browserURL(String webUrl) {  // 3 Crear una nueva instancia de OpenWeb
         return instrumented(OpenWeb.class, webUrl);
     }
 }
